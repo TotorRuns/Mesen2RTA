39e90c65e944629fbcf33b878cace08315ccc95f
108Pi <->
2025-05-15 11:16:41 -0700

    Fixed playing movies

--- Core/Shared/Movies/MesenMovie.cpp
+++ Core/Shared/Movies/MesenMovie.cpp
@@ -137,8 +137,8 @@ bool MesenMovie::Play(VirtualFile &file)
 	ParseSettings(settingsData);
 	
 	string version = LoadString(_settings, MovieKeys::MesenVersion);
-	if(version.size() < 2 || version.substr(0, 2) == "0." || version.substr(0, 2) == "1.") {
-		//Prevent loading movies from Mesen/Mesen-S version 0.x.x or 1.x.x
+	if(version.size() < 2 || version.substr(0, 2) == "1.") {
+		//Prevent loading movies from Mesen/Mesen-S version 1.x.x
 		MessageManager::DisplayMessage("Movies", "MovieIncompatibleVersion");
 		return false;
 	}
